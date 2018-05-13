8c706c54691d25e2a0c3818fa00549935bbbebe2
Christian Kaas <christiankaas@gmail.com>
2018-05-13 14:28:12 -0400

    Added inference of moves.

--- Board.ml
+++ Board.ml
@@ -35,7 +35,7 @@ module Edge =
   struct
     type t = move_type 
     let compare = Pervasives.compare
-    let default = Move
+    let default = Step
     let equal = (=)
   end
 
@@ -51,7 +51,7 @@ module type GAMEBOARD =
   end
 
 
-module Whitechapel : GAMEBOARD =
+module Whitechapel  =
   struct
     
     (* Implements a standard graph structure, and make all the default
@@ -68,8 +68,76 @@ module Whitechapel : GAMEBOARD =
                           let compare = Pervasives.compare
                         end)
 
+    (* Returns the set of vertices reachable from vertex n through an edge
+       of type e, in graph g. Optionally, exclude from results any values
+       already present in set f *)
+    let succ_e (g : Graph.t) (n : Vertex.t) (e : Edge.t)
+               ?(f : S.t = S.empty) () : S.t =
+      let open List in
+      (* Keep track of identified vertices *)
+      let results : S.t ref = ref S.empty in
+      (* Returns a list of all edges between vertices elt and n *)
+      let edges (elt : Vertex.t) : Graph.edge list =
+        Graph.find_all_edges g n elt
+      in
+      (* Filters a list of edges, adding to results set those whose type
+         matches e AND whose destination vertex v2 is NOT in f *)
+      let filter (lst : Graph.edge list) : unit =
+        iter (fun (_, ctype, v2) ->
+                if Edge.equal ctype e && not (S.mem v2 f)
+                  then results := S.add v2 !results else ()) lst
+      in
+      (* List all vertices reachable from n *)
+      let reachable : Vertex.t list = Graph.succ g n in
+      (* Iterate through all vertices reachable from n, adding to results
+         set only those meeting the criteria set out in filter *)
+      iter (fun elt -> filter (edges elt)) (reachable);
+      !results
+
+    (* While the Geometry data does not explicit Circle to Circle moves, we
+       can infer these at load time, and create explicit edges for more
+       efficient traversal *)
+    let pull_circles (g : Graph.t) (n : Vertex.t) : S.t =
+      (* Keep track of visited vertices *)
+      let visited = ref (S.singleton n) in
+      (* Define a shorthand for unvisited successors of a vertex *)
+      let next_vs (n : Vertex.t) = succ_e g n Step ~f:(!visited) () in
+      (* Keep track of remaining candidates to visit *)
+      let candidates = ref (next_vs n) in
+      (* Keep track of circles successfully identified *)
+      let result = ref S.empty in
+      (* While the candidate set still has members... *)
+      while not (S.is_empty !candidates) do
+        (* Get the next vertex and move it from the candidate to the
+           visited set *)
+        let cand_0 = S.find_first (fun elt -> true) !candidates in
+        visited := S.add cand_0 !visited;
+        candidates := S.remove cand_0 !candidates;
+        match cand_0 with
+        (* If the vertex is a Circle, add it to result set *)
+        | Circle _ -> result := S.add cand_0 !result;
+        (* If the vertex is a Square, access successor vertices, and add any
+           not yet visited to the candidate set *)
+        | Square _ -> candidates := S.union (next_vs cand_0) !candidates;
+      done;
+      !result
+
+    (* While the Geometry data does not explicit Circle to Circle moves, we
+       can infer these at load time, and create explicit edges for more
+       efficient traversal *)
+    let infer_moves (g : Graph.t) =
+      let open List in
+      let circles = 195 in
+      let vertices = init (circles) (fun x -> circles - x) in
+      let format acc elt =
+        let vertex = Circle elt in
+        let links = S.elements (pull_circles g vertex) in
+        (vertex, links) :: acc
+      in
+      fold_left format [] vertices
+
     (* Loads a dataset of edges into the graph g, assigning type t to the
-       connections made *)
+       edges created *)
     let rec load_geometry (g : Graph.t) (t : Edge.t)
                           (dataset : (Vertex.t * Vertex.t list) list) : unit =
       match dataset with
@@ -78,66 +146,30 @@ module Whitechapel : GAMEBOARD =
           List.iter (fun v2 -> Graph.add_edge_e g (v1, t, v2)) v2_lst;
           load_geometry g t tl
 
-    (* Returns the set of vertices reachable from vertex n through an edge
-       of type e, in graph g. Optionally, exclude from results and values
-       already present in set f *)
-    let succ_e (g : Graph.t)
-               (n : Vertex.t)
-               (e : Edge.t)
-               ?(f : S.t = S.empty) () : S.t =
-      let open Graph in
-      let filter_by_type acc elt =
-        let (_, ctype, v2) = find_edge g n elt in
-        if Edge.equal ctype e then S.add v2 acc else acc
-      in
-      (* For all vertices that are reachable from n in g... *)
-      let reachable = succ g n in
-      (* Return only those reachable through a connection of type e... *)
-      let filtered = List.fold_left (filter_by_type) (S.empty) (reachable) in
-      (* And return only those filtered vertices not already found in f *)
-      S.diff filtered f
+    let make_moves (g : Graph.t) (n : Vertex.t) (c : int) : Vertex.t list = 
+    
 
-    (* Returns a set of Circles in graph g that are reachable within a single
-       move from Circle n *)
-    let pull_circles (g : Graph.t) (n : Vertex.t) (e : Edge.t) : S.t =
-      let open S in
-      let visited = ref (singleton n) in 
-      let rec iter (pend : Vertex.t list) (result : S.t) =
-        match pend with
-        | [] -> result
-        | hd :: tl ->
-            visited := add hd !visited;
-            match hd with
-            | Circle _ -> iter tl (add hd result)
-            | Square _ -> let next_nodes = succ_e g hd e ~f:(!visited) () in
-                          iter ((elements next_nodes) @ pend) result
-      in iter (elements (succ_e g n e ())) (empty)
-
-
-
-    let make_move (g : Graph.t) (n : Vertex.t) (ct : int) : Vertex.t list =
-      let rec move (result : S.t) (count : int) =
-        if count < 0 then raise (Invalid_argument "make_move: invalid move")
-        else if count = 0 then S.elements result
-        else
-          let union = S.union result (S.of_list (pull_circles g n Move)) in 
-          move union (count - 1)
-      in
-      move (S.of_list (pull_circles g n Move)) ct 
 
     let single (g : Graph.t) (n : Vertex.t) : Vertex.t list =
-      make_move g n 1 
+      S.elements (succ_e g n Move ())
 
     let carriage (g : Graph.t) (n : Vertex.t) : Vertex.t list =
-      make_move g n 2
+      let first = single g n in
+      let second_with_overlap =
+        List.fold_left (fun elt -> ) first in 
 
     let alleyway (g : Graph.t) (n : Vertex.t) : Vertex.t list =
-      pull_circles g n Alleyway
+      S.elements (succ_e g n Alleyway ())
 
     let create () : Graph.t =
       let board = Graph.create () in 
+      (* Load steps first *)
+      load_geometry board Step steps;
+      (* Use steps data to infer Move geometry *)
+      load_geometry board Move (infer_moves board);
+      (* Load alleyways last*)
       load_geometry board Alleyway alleyways;
-      load_geometry board Move moves;
+
       board
 
    end
